bae8fa27848d5ede0e1d606cccdeb1b72c417c9b
Suneito <39382727+suneito@users.noreply.github.com>
2024-02-28 20:50:23 +0100

    update for telegram msg

--- src/main/java/cat/babot/Main.java
+++ src/main/java/cat/babot/Main.java
@@ -1,26 +1,31 @@
 package cat.babot;
 
+import cat.babot.com.Telegram;
 import org.jsoup.nodes.Document;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final java.util.logging.Logger atenea = Logger.getLogger("Main");
 
     public static void main(String[] args) throws IOException {
+        Telegram telegram = new Telegram(args[1], args[2], args[3]);
         URL url = new URL("https://www.airlinemanager.com/fuel.php");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestProperty("cookie", args[0]);
         con.setRequestMethod("GET");
-        logger.info(con.getResponseCode() + " " + con.getResponseMessage());
+        atenea.info(con.getResponseCode() + " " + con.getResponseMessage());
         Document document = org.jsoup.Jsoup.parse(getContent(con).toString());
-        logger.info(document.selectXpath("//div[@id='fuelMain']//span[@class='text-danger']").text());
+        String price = document.selectXpath("//div[@id='fuelMain']//span[@class='text-danger']").text();
+        atenea.info(price);
+        telegram.sendMsg(formatMsg(price));
     }
 
     private static StringBuffer getContent(HttpURLConnection con) throws IOException {
@@ -32,6 +37,15 @@ public class Main {
         }
         in.close();
         return content;
+    }
+
+    private static String formatMsg(String price) {
+        StringBuffer content = new StringBuffer();
+        content.append("Last price change:\n")
+                .append(price)
+                .append("\n")
+                .append(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
+        return content.toString();
     }
 
 }
